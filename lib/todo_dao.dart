48ae98143b62df348a97d6fee08e7c44aa7963c1
Adewole Adewumi <wolejr@gmail.com>
2024-11-19 19:43:39 -0500

    added dart documentation

--- lib/todo_dao.dart
+++ lib/todo_dao.dart
@@ -1,15 +1,19 @@
 import 'package:floor/floor.dart';
 import 'todo_item.dart';
 
+/// This is the Data Access Object for TodoItem entity
 @dao
 abstract class ToDoDAO {
 
+  /// This returns all the todoitems.
   @Query("Select * from TodoItem")
   Future<List<TodoItem>> getAllItems();
 
+  /// This inserts into the todo item entity
   @insert
   Future<void> insertItem(TodoItem itm);
 
+  /// This deletes from the todo item entity
   @delete
   Future<void> deleteItem(TodoItem itm);
 
--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -1,11 +1,15 @@
 import 'package:floor/floor.dart';
 
+/// This is the entity that represents a todo item.
 @entity
 class TodoItem {
   static int ID = 1;
 
+  /// This is the unique identifier for this entity.
   @primaryKey
   final int id;
+
+  /// This is the actual item
   final String todoItem;
 
   TodoItem(this.id, this.todoItem) {
